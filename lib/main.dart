ddba9fe2ac452dc90f9552f831f7ee3cd1fa994c
Kaz-glitch <rawoo7762@gmail.com>
2025-12-12 21:47:09 +0300

    شرح التعديل

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'login_selection.dart';
+import 'package:employee/login_pages/login_selection.dart';
 
 void main() {
   runApp(const EmployeeDashboardApp());
